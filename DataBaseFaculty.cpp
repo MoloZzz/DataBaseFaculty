51cd9c5b641e5644c2dc570ba0316ad7bb46a12e
MoloZzz <99015088+MoloZzz@users.noreply.github.com>
2022-10-02 01:31:34 +0300

    ~додав версію на екран

--- DataBaseFaculty.cpp
+++ DataBaseFaculty.cpp
@@ -1,6 +1,4 @@
 ﻿#include <iostream>
-#include <fstream>
-#include <string>
 #include"Structures.h"
 #include"WorkWithFile.h"
 
@@ -8,7 +6,9 @@
 
 
 int main() {
-
+	SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_INTENSITY);
+	std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tData Base Faculty\n\t\t\t\t\t Version 1.4 press any key to start" << std::endl;
+	int waiting = _getch();
 	menu();
 
 
