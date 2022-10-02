2e96b51f2617d261d19990f606b6b83caaf30839
MoloZzz <mega_oleksii@ukr.net>
2022-10-02 14:22:27 +0300

    +додав трохи кольорів

--- DataBaseFaculty.cpp
+++ DataBaseFaculty.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+#include <fstream>
+#include <string>
 #include"Structures.h"
 #include"WorkWithFile.h"
 
@@ -6,9 +8,10 @@
 
 
 int main() {
+
 	SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_INTENSITY);
 	std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tData Base Faculty\n\t\t\t\t\t Version 1.4 press any key to start" << std::endl;
-	int waiting = _getch();
+
 	menu();
 
 
